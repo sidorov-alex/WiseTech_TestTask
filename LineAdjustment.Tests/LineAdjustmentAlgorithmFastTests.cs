151051dd4f702bb34297f7f0c5e6b1714f06b9f4
Alexander Sidorov <salex@fastmail.com>
2025-01-11 02:06:13 +1000

    added Fast realization LineAdjustmentAlgorithmFast

---
+++ LineAdjustment.Tests/LineAdjustmentAlgorithmFastTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace LineAdjustment.Tests
+{
+    public class LineAdjustmentAlgorithmFastTests
+    {
+        [Test]
+        [TestCase(null, 5, "")]
+        [TestCase("", 5, "")]
+        [TestCase("test", 5, "test ")]
+        [TestCase("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 12,
+            "Lorem  ipsum\ndolor    sit\namet        \nconsectetur \nadipiscing  \nelit  sed do\neiusmod     \ntempor      \nincididunt  \nut labore et\ndolore magna\naliqua      ")]
+        [TestCase("Lorem     ipsum    dolor", 17, "Lorem ipsum dolor")]
+        [TestCase("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 120,
+            "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua")]
+        public void Simple(string input, int lineWidth, string expected)
+        {
+            expected = FixNewLines(expected);
+            
+            var algorithm = new LineAdjustmentAlgorithmFast();
+            var output = algorithm.Transform(input, lineWidth);
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestCase("VeryVeryLongWord Is Here", 10, "VeryVeryLo\nngWord  Is\nHere      ")]
+        [TestCase("VeryVeryLongWordPlusPlus Is Here", 10, "VeryVeryLo\nngWordPlus\nPlus    Is\nHere      ")]
+        [TestCase("VeryVeryLongWordPlusPlus AnotherLongWord Is Here", 10, 
+            "VeryVeryLo\nngWordPlus\nPlus      \nAnotherLon\ngWord   Is\nHere      ")]
+        public void NormalizeWordList(string input, int lineWidth, string expected)
+        {
+            expected = FixNewLines(expected);
+            
+            var algorithm = new LineAdjustmentAlgorithmFast();
+            var output = algorithm.Transform(input, lineWidth);
+            Assert.AreEqual(expected, output);
+        }
+        
+        // [TestCase("A B С", 8, "A   B  С")]
+        // public void ComposeLineAndAppend(string input, int lineWidth, string expected)
+        // {
+        //     expected = FixNewLines(expected);
+        //     
+        //     var algorithm = new LineAdjustmentAlgorithm();
+        //     var output = algorithm.Transform(input, lineWidth);
+        //     Assert.AreEqual(expected, output);
+        // }
+        
+        [Test]
+        public void ThrowsArgumentExecptions()
+        {
+            var algorithm = new LineAdjustmentAlgorithmFast();
+            
+            Assert.Throws<ArgumentException>(() => algorithm.Transform("", 0));
+        }
+
+        [Test]
+        public void AlwaysSuccess_JustPrintResultToDebug()
+        {
+            var input = "Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. ";
+            
+            var algorithm = new LineAdjustmentAlgorithmFast();
+            var result = algorithm.Transform(input, 30);
+            
+            Debug.Write(result);
+            
+            // Sed   ut   perspiciatis,  unde
+            // omnis  iste  natus  error  sit
+            // voluptatem         accusantium
+            // doloremque  laudantium,  totam
+            // rem  aperiam  eaque ipsa, quae
+            // ab illo inventore veritatis et
+            // quasi  architecto beatae vitae
+            // dicta  sunt,  explicabo.  Nemo
+            // enim  ipsam  voluptatem,  quia
+            // voluptas  sit,  aspernatur aut
+            // odit   aut   fugit,  sed  quia
+            // consequuntur   magni   dolores
+            // eos,  qui  ratione  voluptatem
+            // sequi  nesciunt,  neque  porro
+            // quisquam   est,   qui  dolorem
+            // ipsum,  quia  dolor sit, amet,
+            // consectetur,  adipisci  velit,
+            // sed quia non numquam eius modi
+            // tempora incidunt, ut labore et
+            // dolore  magnam aliquam quaerat
+            // voluptatem.  Ut enim ad minima
+            // veniam,      quis      nostrum
+            // exercitationem  ullam corporis
+            // suscipit  laboriosam,  nisi ut
+            // aliquid    ex    ea    commodi
+            // consequatur?  Quis  autem  vel
+            // eum iure reprehenderit, qui in
+            // ea  voluptate velit esse, quam
+            // nihil  molestiae  consequatur,
+            // vel  illum,  qui  dolorem  eum
+            // fugiat,   quo  voluptas  nulla
+            // pariatur?   At   vero  eos  et
+            // accusamus    et   iusto   odio
+            // dignissimos    ducimus,    qui
+            // blanditiis         praesentium
+            // voluptatum    deleniti   atque
+            // corrupti, quos dolores et quas
+            // molestias    excepturi   sint,
+            // obcaecati    cupiditate    non
+            // provident,  similique  sunt in
+            // culpa,  qui  officia  deserunt
+            // mollitia animi, id est laborum
+            // et   dolorum  fuga.  Et  harum
+            // quidem  rerum  facilis  est et
+            // expedita    distinctio.    Nam
+            // libero   tempore,  cum  soluta
+            // nobis   est   eligendi  optio,
+            // cumque   nihil   impedit,  quo
+            // minus id, quod maxime placeat,
+            // facere     possimus,     omnis
+            // voluptas  assumenda est, omnis
+            // dolor  repellendus. Temporibus
+            // autem    quibusdam    et   aut
+            // officiis   debitis  aut  rerum
+            // necessitatibus  saepe eveniet,
+            // ut  et  voluptates repudiandae
+            // sint    et    molestiae    non
+            // recusandae. Itaque earum rerum
+            // hic    tenetur    a   sapiente
+            // delectus,  ut  aut  reiciendis
+            // voluptatibus   maiores   alias
+            // consequatur   aut  perferendis
+        }
+        
+        private string FixNewLines(string value)
+        {
+            return value?.Replace("\n", Environment.NewLine);
+        }
+    }
+}
---
+++ LineAdjustment/LineAdjustmentAlgorithmFast.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LineAdjustment
+{
+    public static class RangeExtensions
+    {
+        public static int GetLength(this Range range, int collectionLength)
+        {
+            var (_, length) = range.GetOffsetAndLength(collectionLength);
+            return length;
+        }
+    }
+
+    public class LineAdjustmentAlgorithmFast
+    {
+        public string Transform(string input, int lineWidth)
+        {
+            if (lineWidth <= 0)
+                throw new ArgumentException("Line width should be positive number.", nameof(lineWidth));
+            
+            if (string.IsNullOrWhiteSpace(input))
+                return "";
+
+            var inputSpan = input.AsSpan();
+            
+            Span<Range> words = stackalloc Range[100];
+            var wordsCount = inputSpan.Split(words, ' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            
+            if (wordsCount == 0)
+                return "";
+
+            // Length of each word shouldn't be longer than line width.
+
+            if (true)//words.Any(word => word.Length > lineWidth))
+            {
+                words = NormalizeWordList(words, wordsCount, lineWidth, inputSpan);
+                wordsCount = words.Length;
+            }
+            
+            var sb = new StringBuilder(input.Length * 2); // Enough capacity for only 1 memory allocation
+           
+            var lineLength = 0;
+            var lineWords = new List<Range>(10);
+            
+            //foreach (var word in words)
+            for (var i = 0; i < wordsCount; i++)
+            {
+                var word = words[i];
+                // If we collected enough words then we will compose a line.
+                if (lineLength + word.GetLength(inputSpan.Length) + lineWords.Count > lineWidth)
+                {
+                    if (sb.Length > 0)
+                        sb.AppendLine();
+                    
+                    ComposeLineAndAppend(lineWords, lineWidth, sb, inputSpan);
+                    
+                    lineLength = 0;
+                    lineWords.Clear();
+                }
+                
+                lineWords.Add(word);
+                lineLength += word.GetLength(inputSpan.Length);
+            }
+
+            // Process the last line if any words left.
+            
+            if (lineWords.Count > 0)
+            {
+                if (sb.Length > 0)
+                    sb.AppendLine();
+                
+                ComposeLineAndAppend(lineWords, lineWidth, sb, inputSpan);
+            }
+
+            return sb.ToString();
+        }
+
+        private Span<Range> NormalizeWordList(Span<Range> words, int wordsCount, int lineWidth, ReadOnlySpan<char> inputSpan)
+        {
+            var normalized = new List<Range>(words.Length * 2);
+            
+            //foreach (var word in words)
+            for (var w = 0; w < wordsCount; w++)
+            {
+                var word = words[w];
+                
+                if (word.GetLength(inputSpan.Length) > lineWidth)
+                {
+                    // Word can be longer than 2+ lineWidth.
+                    var parts = word.GetLength(inputSpan.Length) / lineWidth;
+
+                    var i = 0;
+                    for (; i < parts; i++)
+                    {
+                        normalized.Add(new Range(word.Start.Value + i * lineWidth,
+                            word.Start.Value + i * lineWidth + lineWidth));
+                        //normalized.Add(word.Substring(i * lineWidth, lineWidth));
+                    }
+                    
+                    normalized.Add(new Range(word.Start.Value + i * lineWidth, word.End.Value));
+                    //normalized.Add(Range.StartAt(word.Start.Value + i * lineWidth));
+                    //normalized.Add(word.Substring(i * lineWidth));
+                }
+                else
+                {
+                    normalized.Add(word);
+                }
+            }
+
+            return normalized.ToArray();
+        }
+
+        private void ComposeLineAndAppend(List<Range> words, int lineWidth, StringBuilder sb, ReadOnlySpan<char> inputSpan)
+        {
+            int inputLength = inputSpan.Length;
+            
+            // Расстояние между словами нужно заполнять равным количеством пробелов, если же это не возможно, то добавляем
+            // еще по пробелу между словами слева направо. Если в строке помещается только 1 слово, то дополнить строку 
+            // пробелами справа.
+
+            if (words.Count == 1)
+            {
+                sb.Append(inputSpan[words[0]]);
+                sb.Append(' ', lineWidth - words[0].GetLength(inputLength));
+                return;
+            }
+            
+            var total = lineWidth - words.Sum(w => w.GetLength(inputLength));
+            var equal = total / (words.Count - 1); // Spaces between words we always add
+            var extra = total % (words.Count - 1); // Extra spaces that we'll use to add from left to right 
+
+            for (var i = 0; i < words.Count - 1; i++)
+            {
+                sb.Append(inputSpan[words[i]]);
+                sb.Append(' ', equal);
+
+                if (extra != 0)
+                {
+                    sb.Append(' ');
+                    extra--;
+                }
+            }
+            
+            // Last word we add without spaces after.
+            sb.Append(inputSpan[words[^1]]);
+        }
+    }
+}
